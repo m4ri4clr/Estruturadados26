043e24d8e4be5270939e2702a61ed4768873bc4b
Maria Clara <mariaclrif@gmail.com>
2026-08-17 19:37:23 -0300

    Exercício 2 - palíndromo

---
+++ Vetores/Vetores/Exercícios/2/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+int qtdeletras = palavra.Length;
+int[] Array = new int[qtdeletras];
+int[] invertida = new int[qtdeletras];
+int palindromo = 1;
+
+for (int i = 0; i < qtdeletras; i++)
+{
+    Array[i] = (int)palavra[i];
+}
+
+for (int i = 0; i < qtdeletras; i++)
+{
+    invertida[i] = Array[qtdeletras - 1 - i];
+}
+
+for (int i = 0; i < qtdeletras; i++)
+{
+    if (Array[i] != invertida[i])
+    {
+        palindromo = 0;
+    }
+}
+
+if (palindromo == 1)
+{
+    Console.WriteLine("A palavra é um palíndromo!");
+}
+else
+{
+    Console.WriteLine("A palavra não é um palíndromo.");
+}
